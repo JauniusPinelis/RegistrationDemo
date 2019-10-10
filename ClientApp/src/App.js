7829ccf78a057fe3c20b9c13828fa5156c13257f
Jaunius Pinelis <JauniusPinelis@gmail.com>
2019-10-10 08:32:46 +0300

    Added Alert on successfull submit

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -6,6 +6,7 @@ import axios from "axios";
 // React-bootstrap
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
+import Alert from "react-bootstrap/Alert";
 
 // Components
 import QuestionForm from "./components/QuestionForm";
@@ -16,20 +17,21 @@ class App extends Component {
     super();
     this.state = {
       questions: [],
-      disabled: true
+      disabled: true,
+      showAlert: false
     };
-  };
+  }
   updateDisabled = () => {
     this.setState({
       disabled: !this.state.disabled
-    })
-  }
-  handleAnswerChange = (e) => {
+    });
+  };
+  handleAnswerChange = e => {
     let controlName = e.target.name;
     let value = e.target.value;
 
     this.setState(state => {
-      const updatedQuestions = state.questions.map((question) => {
+      const updatedQuestions = state.questions.map(question => {
         if (question.name === controlName) {
           question.value = value;
           return question;
@@ -41,33 +43,40 @@ class App extends Component {
         updatedQuestions
       };
     });
-    
   };
   loadData = () => {
     axios
       .get("/api/registrations/")
       .then(res => {
         this.setState({
-          questions: res.data,
+          questions: res.data
         });
       })
       .catch(error => {});
   };
   clearData = () => {
     this.loadData();
-  }
+  };
+  onShowAlert = ()=>{
+    this.setState({showAlert:true},()=>{
+      window.setTimeout(()=>{
+        this.setState({showAlert:false})
+      },2000)
+    });
+  };
   submitAnswers = () => {
-     axios({
+    axios({
       headers: {
         "Content-Type": "application/json"
       },
       method: "post",
       url: "/api/registrations/",
       data: this.state.questions
-    }).then(res=> {
+    }).then(res => {
+      this.onShowAlert();
       this.loadData();
     });
-  }
+  };
   componentDidMount = () => {
     this.loadData();
   };
@@ -76,12 +85,22 @@ class App extends Component {
       <div className="App">
         <Container>
           <h3>Registracijos Forma</h3>
+          <Alert show={this.state.showAlert} variant="success">
+             Submit has been successfull
+            </Alert>
           <div className="form">
             <Form>
-              <QuestionForm handleChange={this.handleAnswerChange} disabled={this.state.disabled} 
-              questionData={this.state.questions} />
-              <ButtonMenu disabled={this.state.disabled} updateDisabled={this.updateDisabled} 
-              submitAnswers={this.submitAnswers} clearData={this.clearData} />
+              <QuestionForm
+                handleChange={this.handleAnswerChange}
+                disabled={this.state.disabled}
+                questionData={this.state.questions}
+              />
+              <ButtonMenu
+                disabled={this.state.disabled}
+                updateDisabled={this.updateDisabled}
+                submitAnswers={this.submitAnswers}
+                clearData={this.clearData}
+              />
             </Form>
           </div>
         </Container>
